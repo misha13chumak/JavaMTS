d85d48c6cc91a01f4fc9e28f0192db22bbce50b1
misha13chumak <misha.chumak13@gmail.com>
2023-12-14 11:48:41 +0300

    test gitignore

---
+++ src/main/java/ru/mts/Purchase.java
@@ -0,0 +1,32 @@
+package ru.mts;
+
+// Purchase.java
+
+
+public class Purchase {
+    // Количество товаров
+    private int itemCount;
+
+    // Сумма товара без скидки
+    private double totalAmount;
+
+    // Скидка на товар
+    private double discount;
+
+
+    public Purchase(int itemCount, double totalAmount, double discount) {
+        this.itemCount = itemCount;
+        this.totalAmount = totalAmount;
+        this.discount = discount;
+    }
+
+
+    public void calculateTotal() {
+        double totalWithoutDiscount = itemCount * totalAmount;
+        double discountedTotal = totalWithoutDiscount * (1 - discount / 100);
+
+        System.out.println("Общая сумма покупки без скидки: " + totalWithoutDiscount);
+        System.out.println("Общая сумма покупки со скидкой: " + String.format("%.2f", discountedTotal));
+    }
+}
+
